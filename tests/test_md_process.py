163a5bbc2478c5f90b418253ca8540f2fde15f95
Daniel <dcownden@gmail.com>
2023-08-22 11:58:10 -0700

    Update test_md_process.py

--- tests/test_md_process.py
+++ tests/test_md_process.py
@@ -54,7 +54,7 @@ def test_default_embedding(cmd):
     assert len(nb.cells) == 4
     assert nb.cells[0].cell_type == 'markdown'
     assert "Default Embedding Test" in nb.cells[0].source
-    assert '<iframe src="https://jackliddy.github.io/designTest1" width="600" height="400"></iframe>' in nb.cells[1].source
+    assert '<iframe src="https://jackliddy.github.io/designTest1" width="600" height="400"></iframe>' in nb.cells[2].source
 
 def test_custom_dimension_embedding(cmd):
     ipynb_content = md_to_ipynb_conversion_test(cmd, "custom_dimension_embedding_test.md")
@@ -63,7 +63,7 @@ def test_custom_dimension_embedding(cmd):
     assert len(nb.cells) == 4
     assert nb.cells[0].cell_type == 'markdown'
     assert "Custom Dimension Embedding Test" in nb.cells[0].source
-    assert '<iframe src="https://jackliddy.github.io/designTest1" width="800" height="500"></iframe>' in nb.cells[1].source
+    assert '<iframe src="https://jackliddy.github.io/designTest1" width="800" height="500"></iframe>' in nb.cells[2].source
 
 def test_regular_link(cmd):
     ipynb_content = md_to_ipynb_conversion_test(cmd, "regular_link_test.md")
@@ -72,6 +72,6 @@ def test_regular_link(cmd):
     assert len(nb.cells) == 4
     assert nb.cells[0].cell_type == 'markdown'
     assert "Regular Link Test" in nb.cells[0].source
-    assert '[This should remain a regular link](https://jackliddy.github.io/designTest1)' in nb.cells[0].source
+    assert '[This should remain a regular link](https://jackliddy.github.io/designTest1)' in nb.cells[2].source
 
 # ... Add more tests as needed
